16740ce45b3a1269d7f690b5f8de7e8f96e8e9c7
kaife0 <mdkaif1590@gmail.com>
2025-05-03 12:49:50 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // Ensure assets are loaded using relative paths
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
